0debbd04dc92e498db9df391e10b52d1098b0f24
J.W <jakwings@gmail.com>
2018-05-22 12:18:46 +0800

    Use fewer predefined search paths for SDL2

--- FeLib/FindSDL2.cmake
+++ FeLib/FindSDL2.cmake
@@ -66,14 +66,10 @@
 #  License text for the above reference.)
 
 SET(SDL2_SEARCH_PATHS
-	~/Library/Frameworks
 	/Library/Frameworks
-	/usr/local
 	/usr
-	/sw # Fink
-	/opt/local # DarwinPorts
-	/opt/csw # Blastwave
-	/opt
+        # other paths like ~/Library/Frameworks and /usr/local
+        # should be provided by $ENV{SDL2DIR}
 )
 
 # CPU architecture detection for MSVC
