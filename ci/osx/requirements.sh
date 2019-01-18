8abb51efadd546db5a88420891ab4fcfd6f2c664
J.W <jakwings@gmail.com>
2019-01-18 21:42:39 +0800

    Distribute mac app with dmg file
    Pros: mount it as read-only drive & play ivan in place & save disk space

--- ci/osx/requirements.sh
+++ ci/osx/requirements.sh
@@ -20,8 +20,8 @@ brew_install() {
 
 install_sdl2() {
   local urls=(
-    'https://libsdl.org/release/SDL2-2.0.8.dmg'
-    'https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.2.dmg'
+    'https://libsdl.org/release/SDL2-2.0.9.dmg'
+    'https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.4.dmg'
   )
 
   mkdir -p "${CACHE_DIR}"
